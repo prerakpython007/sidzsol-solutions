eeda0bfe030c9b49d8e9596fdf9916754c78038e
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-06-10 13:20:35 +0530

    .

---
+++ src/app/Pages/AboutPage/page.tsx
@@ -0,0 +1,12 @@
+import { GlobeDemo } from '@/app/components/Globe'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <GlobeDemo/>
+    </div>
+  )
+}
+
+export default page
